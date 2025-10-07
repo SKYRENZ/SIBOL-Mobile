04fe9f02b2c2a3e9e2c79246b2b7acf93d653d94
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-07 22:06:45 +0800

    fixed web error dependencies

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3b1212ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
