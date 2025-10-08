4775a9e5db2b79fc27e253e4a4b33d8da0c6b85c
Lair027 <samislaircoleenw@gmail.com>
2025-10-09 00:47:43 +0800

    dashboard household, tailwind setup, added assets and component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Dashboard from './lib/Pages/Dashboard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Dashboard />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#3b1212ff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
